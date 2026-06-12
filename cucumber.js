896fef799b0a8a27fa95da117395437d77ce0a55
Piotr Spychała <piotr.spychala@strix.net>
2026-06-12 16:40:20 +0200

    GA-1: adressed comments 2

--- cucumber.js
+++ cucumber.js
@@ -2,9 +2,6 @@ const TAGS = process.env.TAGS
 
 export default {
     parallel: 1,
-    format: ['allure-cucumberjs/reporter',
-      // 'summary' // conflicts with Allure, disable for regression runs
-    ],
     requireModule: ['ts-node/register'],
     require: [
       './features/step-definitions/**/*',
