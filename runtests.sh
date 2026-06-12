fcb2d8742eb4a203ff5f04d00a7798228b7e7e16
Piotr Spychała <piotr.spychala@strix.net>
2026-06-12 16:03:23 +0200

    GA-1: Added cucumber execution and browser combinations to shell script

--- runtests.sh
+++ runtests.sh
@@ -9,7 +9,13 @@ echo "
 # Device list: desktopHD, desktop1440p, iphone14ProMax, galaxyS20Ultra
 # Browser list: chrome, safari, firefox
 # Regression: @TESTPLAN_AM2S-5026
-ENV="stage" DEVICE="desktopHD" BROWSERS=("chrome" "safari" "firefox") HEADLESS="true" TAGS=""
+ENV="stage" DEVICE="desktopHD" BROWSERS=("chrome" "safari" "firefox") HEADLESS="false" TAGS="@Regression"
+
+# Run tests for each browser combination
+for BROWSER in "${BROWSERS[@]}"; do
+  ENV=$ENV DEVICE=$DEVICE BROWSER=$BROWSER HEADLESS=$HEADLESS TAGS="$TAGS" \
+  npx cucumber-js
+done
 
 echo "
         ===============================================
