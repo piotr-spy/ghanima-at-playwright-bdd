253723357cee0a01f2f148f87e9a18d934a8b803
Piotr Spychała <piotr.spychala@strix.net>
2026-06-12 15:57:35 +0200

    GA-1: added base URL to environments

--- features/config/environments.ts
+++ features/config/environments.ts
@@ -9,7 +9,7 @@ export const environments = {
         baseURL: ``
     },
     stage: {
-        baseURL: ``
+        baseURL: `https://practicesoftwaretesting.com`
     },
     prod: {
         baseURL: ``
