2b8cc3fcbb4e11fd446ec2151c1e2c7022a58d76
Piotr Spychała <piotr.spychala@strix.net>
2026-06-15 15:45:52 +0200

    GA-3: Re-added PW default configuration scoped to API tests only

---
+++ playwright.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './api/tests',
+  reporter: [
+    ['allure-playwright', { resultsDir: 'allure-results/api' }]
+  ],
+  use: {
+    baseURL: '',
+  },
+})
